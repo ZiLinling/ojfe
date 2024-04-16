937ebb3931fcdf68772834e0f0fc9472b0b7afd1
ZiLinling <2160788947@qq.com>
2024-04-16 14:08:59 +0800

    初始提交

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const pages = {
+  oj: {
+    entry: "./src/pages/oj/index.js",
+    template: "./public/index.html",
+    filename: "index.html",
+    title: "oj"
+  },
+  // admin: {
+  //   entry: './src/pages/admin/index.js',
+  // },
+}
 module.exports = defineConfig({
+  pages: pages,
   transpileDependencies: true
 })
